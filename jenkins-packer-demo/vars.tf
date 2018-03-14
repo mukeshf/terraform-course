ec3addd9871e50ddfdd382ff10cb50b394e89021
Mukesh Fulewale <mukeshfulewale@gmail.com>
2018-03-14 16:55:31 +0530

    Update vars.tf

--- jenkins-packer-demo/vars.tf
+++ jenkins-packer-demo/vars.tf
@@ -2,10 +2,10 @@ variable "AWS_REGION" {
   default = "eu-west-1"
 }
 variable "PATH_TO_PRIVATE_KEY" {
-  default = "mykey"
+  default = "mykeytest1"
 }
 variable "PATH_TO_PUBLIC_KEY" {
-  default = "mykey.pub"
+  default = "mykeytest1.pub"
 }
 variable "AMIS" {
   type = "map"
